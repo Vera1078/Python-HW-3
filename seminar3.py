8b253ff4386b157770602e480cf4ac74cf4c9a85
Vera Lippgardt <vera1078@mail.ru>
2022-10-18 19:39:33 +0300

    Задачи 4  из домашнего задания к семинару 3 по Python

--- seminar3.py
+++ seminar3.py
@@ -51,26 +51,40 @@
 
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+# print('Задача 3. Разница между max и min значением дробной части элементов.')
+# from random import uniform
 
-from random import uniform
+# n = int(input('Введите размер списка '))
+# list1 = []
+# for i in range(n):
+#     f = uniform(0, 9)
+#     list1.append(round(f, 2))
 
+# new_lst = [round(i%1, 2) for i in list1]
+# print(list1, '=>', max(new_lst) - min(new_lst))
 
-n = int(input('Введите размер списка '))
-list1 = []
-for i in range(n):
-    f = uniform(0, 9)
-    list1.append(round(f, 2))
+# print()
+# print()
 
-min = list1[0]
-max = 0
-for i in range(len(list1)):
-    
-    if max < list1[i]:
-        max = list1[i]
-    if min > list1[i]:
-        min = list1[i]
-dif = (max - int(max)) - (min - int(min))
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример:
 
-print(list1)
-print(max, min)
-print(round(dif,2))
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+print('Задача 4. Преобразование десятичного числа в двоичное.')
+a = int(input('Введите число '))
+b = ''
+while a > 0:
+    b = str(a%2) + b
+    a = a // 2
+print(b)
+
+# Задайте число. Составьте список чисел Фибоначчи, 
+# в том числе для отрицательных индексов.
+# Пример:
+
+# - для k = 8 список будет выглядеть так: 
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
+
